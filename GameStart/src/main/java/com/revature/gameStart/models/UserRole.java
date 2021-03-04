0272dcd2d8f6b5843096510e4ff11b747ca9c8fd
Gabrielle Luna <lunagabrielled@gmail.com>
2021-03-04 12:04:15 -0800

    started on models

--- GameStart/src/main/java/com/revature/gameStart/models/UserRole.java
+++ GameStart/src/main/java/com/revature/gameStart/models/UserRole.java
@@ -1,4 +1,66 @@
 package com.revature.gameStart.models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
 public class UserRole {
+    //Attributes ----------------------------------------------------
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, name = "role_name")
+    private String name;
+
+    //Constructors --------------------------------------------------
+    public UserRole() {
+    }
+
+    public UserRole(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    //Getters and Setters -------------------------------------------
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Other ---------------------------------------------------------
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRole userRole = (UserRole) o;
+        return id == userRole.id && Objects.equals(name, userRole.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
