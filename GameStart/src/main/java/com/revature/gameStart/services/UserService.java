208f3ee0744ebc71f2e44f691a3a49af8809b67c
Daniel Skwarcha <daniel.skwarcha@revature.net>
2021-03-07 15:03:09 -0500

    Added more test cases for UserService. All possible tests for register and getUserByUsername are finished except for adding the user due to exception that is being thrown that can't be fixed at the moment.

--- GameStart/src/main/java/com/revature/gameStart/services/UserService.java
+++ GameStart/src/main/java/com/revature/gameStart/services/UserService.java
@@ -2,23 +2,28 @@ package com.revature.gameStart.services;
 
 import com.revature.gameStart.dtos.Principal;
 import com.revature.gameStart.exceptions.InvalidRequestException;
+import com.revature.gameStart.exceptions.ResourceNotFoundException;
+import com.revature.gameStart.exceptions.ResourcePersistenceException;
 import com.revature.gameStart.models.User;
 import com.revature.gameStart.models.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 @Service
 @Transactional
 public class UserService {
 
+    public List<User> users = new ArrayList<>();
+
     @Autowired
     public UserService() {
         super();
+        users.add(new User(1, "Apple", "Pie", "AP", "Pass", "ap@amurica.com", UserRole.BASIC));
+        users.add(new User(2, "Banana", "Split", "BS", "Pass", "bs@amurica.com", UserRole.BASIC));
+        users.add(new User(3, "Chocolate", "Cake", "CC", "Pass", "Cc@amurica.com", UserRole.BASIC));
     }
 
     public User getUserById(int id){
@@ -30,6 +35,13 @@ public class UserService {
     }
 
     public void register(User newUser){
+        if (!isUserValid(newUser)) throw new InvalidRequestException();
+
+        if (getUserByUsername(newUser.getUsername()) != null) {
+            throw new ResourcePersistenceException("Username is already in use");
+        }
+
+        users.add(newUser);
 
     }
 
@@ -43,9 +55,13 @@ public class UserService {
         return null;
     }
 
-    public User getUserByUsername(){
-
-        return null;
+    public User getUserByUsername(String username){
+        if (username == null || username.trim().equals("")) {
+            throw new InvalidRequestException();
+        }
+        for(User user : users)
+            if (user.getUsername().equals(username)) return user;
+        throw new ResourceNotFoundException();
     }
 
     public void confirmAccount(){
@@ -65,8 +81,13 @@ public class UserService {
 
     }
 
-    public Boolean isUserValid(){
-        return false;
+    public Boolean isUserValid(User user){
+        if (user == null) return false;
+        if (user.getFirstName() == null || user.getFirstName().trim().equals("")) return false;
+        if (user.getLastName() == null || user.getLastName().trim().equals("")) return false;
+        if (user.getUsername() == null || user.getUsername().trim().equals("")) return false;
+        if (user.getPassword() == null || user.getUsername().trim().equals("")) return false;
+        return true;
     }
 
 
--- GameStart/src/test/java/com/revature/gameStart/services/UserServiceTest.java
+++ GameStart/src/test/java/com/revature/gameStart/services/UserServiceTest.java
@@ -2,16 +2,20 @@ package com.revature.gameStart.services;
 
 
 import com.revature.gameStart.exceptions.InvalidRequestException;
+import com.revature.gameStart.exceptions.ResourceNotFoundException;
+import com.revature.gameStart.exceptions.ResourcePersistenceException;
 import com.revature.gameStart.models.User;
 import com.revature.gameStart.models.UserRole;
 import static org.junit.Assert.*;
+
+import io.micrometer.core.instrument.config.validate.Validated;
 import org.junit.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceTest {
-    public List<User> users = new ArrayList<>();
+
 
 // I believe the way that the unit tests work is you set your unit test up, you call the method, and then you assert your expected result. I believe you should do that and then write your methods to make sure
     // they return what you need. You can start simple by returning exactly what it expects and then write your method to be more complex and become what you actually want it to do. This way you DO NOT write your
@@ -21,23 +25,22 @@ public class UserServiceTest {
     @Before
     public void setUp() {
         this.userService = new UserService();
-        users.add(new User(1, "Apple", "Pie", "AP", "Pass", "ap@amurica.com", UserRole.BASIC));
-        users.add(new User(2, "Banana", "Split", "BS", "Pass", "bs@amurica.com", UserRole.BASIC));
-        users.add(new User(3, "Chocolate", "Cake", "CC", "Pass", "Cc@amurica.com", UserRole.BASIC));
+
     }
 
     @After
     public void tearDown() {
-        users.clear();
+        userService = null;
+
     }
 
-    // Test getUserById
+    // --------------------------------------------------------------------- getUserById----------------------------------------------------------------------------
     // Test that I get the right user and they equal
     @Test
     public void UserBanana() {
 
         //Arrange
-        User user = users.get(2);
+        User user = userService.users.get(2);
         //Act
         User testUser = userService.getUserById(2);
 
@@ -50,7 +53,7 @@ public class UserServiceTest {
     public void UserApple() {
 
         //Arrange
-        User user = users.get(1);
+        User user = userService.users.get(1);
         //Act
         User testUser = userService.getUserById(2);
 
@@ -63,7 +66,7 @@ public class UserServiceTest {
     public void UserIdLessThanZero() {
 
         //Arrange
-        User testUser = users.get(1);
+        User testUser = userService.users.get(1);
         try {
 
             //Act
@@ -75,8 +78,83 @@ public class UserServiceTest {
         }
     }
 
-    // Test Register
+    //---------------------------------------------------------getUserByUsername------------------------------------------------------------------------------------------------------------
+    // Test that a user is returned
+    @Test
+    public void UserAP() {
+        // Arrange
+        String username = "AP";
+        // Act
+        User ap = userService.getUserByUsername(username);
+        // Assert
+        assertNotNull(ap);
+    }
+    // Test that InvalidRequestException is thrown if username is null
+    @Test(expected = InvalidRequestException.class)
+    public void UsernameIsNull() {
 
+        // Act
+        User user = userService.getUserByUsername(null);
 
+        // Assert
+        // Don't have to assert because an exception is expected
+    }
+    // Test that InvalidRequestException is thrown if username is empty string
+    @Test(expected = InvalidRequestException.class)
+    public void UsernameIsEmptyString() {
+
+        // Arrange
+        String username = "";
+        // Act
+        User user = userService.getUserByUsername(username);
+
+        // Assert
+        // Don't have to assert because an exception is expected
+    }
+    @Test(expected = ResourceNotFoundException.class)
+    public void UserNotFound() {
+        // Arrange
+        String username = "BB";
+        // Act
+        User user = userService.getUserByUsername(username);
+
+        // Assert
+        // Don't have to assert because an exception is expected
+    }
+
+    // -------------------------------------------------------Register User-----------------------------------------------------------------------------------------------------------------------
+    // Test that a user is added to the list
+    @Test
+    public void UserRegistered() {
+        // Arrange
+        User user = new User(4, "User4", "Last4", "us4", "ps4","us4@email.com", UserRole.BASIC);
+        // Act
+        userService.register(user);
+        // Assert
+        assertTrue(userService.users.contains(user));
+    }
+
+    // Test that an InvalidRequestException is thrown
+    @Test(expected = InvalidRequestException.class)
+    public void UserNotRegisteredInvalidRequestException() {
+        // Arrange
+        User user = null;
+        // Act
+        userService.register(user);
+        // Assert
+        // Already asserted near @Test. We are expecting an InvalidRequestException
+    }
+
+    // Test that a ResourcePersistenceException is thrown
+    @Test(expected = ResourcePersistenceException.class)
+    public void UsernameInUse() {
+        // Arrange
+        User user = new User(6, "NewUser", "NewLastUser", "AP", "AnyPass", "NewU@email.com", UserRole.BASIC);
+        // Act
+        userService.register(user);
+
+        // Assert
+        // Already asserted new @Test. We are expecting a ResourcePersistenceException
+    }
 
 }
