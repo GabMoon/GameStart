e13ea69b961b3c9219f5e13b05ec6308f2d3917b
Tuan <tuanmai204@gmail.com>
2021-03-08 09:51:07 -0600

    Revert "Api"

--- GameStart/src/main/java/com/revature/gameStart/GameStartDriver.java
+++ GameStart/src/main/java/com/revature/gameStart/GameStartDriver.java
@@ -1,23 +1,7 @@
 package com.revature.gameStart;
 
-import com.revature.gameStart.api.RawgApi;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-@SpringBootApplication
 public class GameStartDriver {
-
-    @Bean
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
-
     public static void main(String[] args) {
-
-        SpringApplication.run(GameStartDriver.class, args);
-
 
     }
 }
