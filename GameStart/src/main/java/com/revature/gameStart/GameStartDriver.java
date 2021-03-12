8d31d8a45fa66b545dd2be1a24c6e86ebc515d87
Gabrielle Luna <lunagabrielled@gmail.com>
2021-03-12 09:36:08 -0800

    Access-Control-Allow-Origin: *

--- GameStart/src/main/java/com/revature/gameStart/GameStartDriver.java
+++ GameStart/src/main/java/com/revature/gameStart/GameStartDriver.java
@@ -27,7 +27,7 @@ public class GameStartDriver {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/games").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/games").allowedOrigins("*");
             }
         };
     }
