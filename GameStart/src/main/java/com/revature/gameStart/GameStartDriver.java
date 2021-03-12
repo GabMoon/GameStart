cf18fabea8749051e912ceb969e01ba9977c3569
Gabrielle Luna <lunagabrielled@gmail.com>
2021-03-11 18:42:04 -0800

    test Access-Control-Allow-Origin

--- GameStart/src/main/java/com/revature/gameStart/GameStartDriver.java
+++ GameStart/src/main/java/com/revature/gameStart/GameStartDriver.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -20,6 +22,15 @@ public class GameStartDriver {
         SpringApplication.run(GameStartDriver.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
 }
 
 
