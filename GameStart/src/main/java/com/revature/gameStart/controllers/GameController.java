99c7325d583d2e9a4364eac8fd99abec46e0595e
tuan <tuan.mai@revature.net>
2021-03-12 16:22:40 -0600

    Game controller is done!

--- GameStart/src/main/java/com/revature/gameStart/controllers/GameController.java
+++ GameStart/src/main/java/com/revature/gameStart/controllers/GameController.java
@@ -27,54 +27,18 @@ public class GameController {
     //Get -----------------------------------------------------------
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Game> getAllGames(){
-//        List<Developer> devs = new ArrayList<>();
-//        List<Publisher> pubs = new ArrayList<>();
-//        List<Platform> plats = new ArrayList<>();
-//        List<Genre> genres = new ArrayList<>();
-//
-//        List<Game> games = new ArrayList<>();
-//
-//        devs.add(new Developer(1, "AppleDeveloper"));
-//        pubs.add(new Publisher(1, "BananaPublisher"));
-//        plats.add(new Platform(1, "CakePlatform"));
-//        genres.add(new Genre(1, "DogmaGenre"));
-//
-//        games.add(new Game("Apple", genres, "Description", 6, devs, pubs, plats));
-//        return games;
+
         return gameService.getAllGames();
     }
 
     @GetMapping(path = "/id/{id}")
     public Game getGameById(@PathVariable int id){
-//        List<Developer> devs = new ArrayList<>();
-//        List<Publisher> pubs = new ArrayList<>();
-//        List<Platform> plats = new ArrayList<>();
-//        List<Genre> genres = new ArrayList<>();
-//
-//        devs.add(new Developer(1, "AppleDeveloper"));
-//        pubs.add(new Publisher(1, "BananaPublisher"));
-//        plats.add(new Platform(1, "CakePlatform"));
-//        genres.add(new Genre(1, "DogmaGenre"));
-//
-//        Game gameWithNameApple = new Game("Apple", genres, "Description", 6, devs, pubs, plats);
-//        return gameWithNameApple;
+
         return gameService.getGameById(id);
     }
 
     @GetMapping(path = "/name/{name}")
     public Game getGameByName(@PathVariable String name){
-//        List<Developer> devs = new ArrayList<>();
-//        List<Publisher> pubs = new ArrayList<>();
-//        List<Platform> plats = new ArrayList<>();
-//        List<Genre> genres = new ArrayList<>();
-//
-//        devs.add(new Developer(1, "AppleDeveloper"));
-//        pubs.add(new Publisher(1, "BananaPublisher"));
-//        plats.add(new Platform(1, "CakePlatform"));
-//        genres.add(new Genre(1, "DogmaGenre"));
-//
-//        Game gameWithNameApple = new Game("Apple", genres, "Description", 6, devs, pubs, plats);
-//        return gameWithNameApple;
 
         return gameService.getGameByName(name);
     }
