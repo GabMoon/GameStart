6a79cd89f885c2c7db6c97971a36ceac3c3675e5
Gabrielle Luna <lunagabrielled@gmail.com>
2021-03-12 19:25:00 -0800

    broaden CORS rules

--- GameStart/src/main/java/com/revature/gameStart/GameStartDriver.java
+++ GameStart/src/main/java/com/revature/gameStart/GameStartDriver.java
@@ -27,7 +27,7 @@ public class GameStartDriver {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/games").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
